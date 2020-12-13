86a4715341660a06521d6ae67d6f8935cd5fed05
InDuck <idk0919@naver.com>
2020-12-13 23:10:27 +0900

    feat: MessageDisplay

---
+++ src/components/UnitTestExamples/MessageDisplay.vue
@@ -0,0 +1,28 @@
+<template>
+  <p v-if="error" data-testid="message-error">{{ error }}</p>
+  <p v-else data-testid="message">{{ message.text }}</p>
+</template>
+
+<script>
+import { getMessage } from '@/services/axios.js';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MessageDisplay',
+  data() {
+    return {
+      message: {},
+      error: null,
+    };
+  },
+  async created() {
+    try {
+      this.message = await getMessage();
+    } catch (error) {
+      this.error = 'Oops! Something went wrong.';
+    }
+  },
+});
+</script>
+
+<style scoped lang="ts"></style>
