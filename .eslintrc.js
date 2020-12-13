59f5450490b49feaed24ad88e25a6aa9eb7fe70a
InDuck <idk0919@naver.com>
2020-12-13 14:46:53 +0900

    fix: prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'max-len': 'off',
+    // 'prettier/prettier': 'off',
+    // 'newline-per-chained-call': 'error',
   },
   overrides: [
     {
